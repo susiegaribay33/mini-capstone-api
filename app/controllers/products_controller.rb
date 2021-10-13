dee30eb5a38ca2d408ba7a6d2481fc2a2a1d0719
SusieG <susiegaribay33@gmail.com>
2021-10-12 20:51:04 -0500

    create db, add all products route, add faker

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,8 @@
 class ProductsController < ApplicationController
+  def products_all
+    # products = Product.all
+    # render json: products.as_json
+    render json: {message: "products"}
+  end
+
 end
