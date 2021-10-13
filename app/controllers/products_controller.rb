652e31644d7ab5315cbd8f32fc84acce8a2f288a
SusieG <susiegaribay33@gmail.com>
2021-10-12 21:32:40 -0500

    add seed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,7 @@
 class ProductsController < ApplicationController
   def products_all
-    # products = Product.all
-    # render json: products.as_json
-    render json: {message: "products"}
+    products = Product.all
+    render json: products.as_json
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  product1 = Product.new(name: Faker::Coffee.blend_name, price: Faker::Number.decimal(l_digits: 2), image_url: Faker::Internet.url(host: 'example.com', path: '/foobar.jpg'), description: Faker::Coffee.notes)
+  product1.save
+end
