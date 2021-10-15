5b25415817394ae8e9c64d38aa19ec0ab8754ab5
SusieG <susiegaribay33@gmail.com>
2021-10-14 21:33:56 -0500

    web-based CRUD and conventions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,41 @@
 class ProductsController < ApplicationController
-  def products_all
+  # def products_all
+  #   products = Product.all
+  #   render json: products.as_json
+  # end
+
+  def index
     products = Product.all
     render json: products.as_json
+  end
+
+  def show
+    requested_id = params[:id]
+    product = Product.find_by(id: requested_id)
+    render json: product.as_json
+  end
+
+  def create
+    the_name = params[:name]
+    the_price = params[:price]
+    the_imageurl = params[:image_url]
+    the_desc = params[:description]
+
+    new_product = Product.new(
+      name: the_name, 
+      price: the_price, 
+      image_url: 
+      the_imageurl, description: 
+      the_desc
+    )
+
+    new_product.save
+    render json: new_product.as_json
+  end
+
+  def update
+    new_thing = params[]
+    render json: {message: "youre in update"}
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/all_products", controller: "products", action: "products_all"
+  # get "/all_products", controller: "products", action: "products_all"
+  get "/products" => "products#index"
+  get "/:id" => "products#show"
+  post "/products" => "products#create"
+  patch "/products/:id" => "products#update"
 end
