aab98ec135995c01a7b3da28f3f139fd1aaf53b2
SusieG <susiegaribay33@gmail.com>
2021-10-19 21:35:22 -0500

    seed dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-10.times do
-  product1 = Product.new(name: Faker::Coffee.blend_name, price: Faker::Number.decimal(l_digits: 2), image_url: Faker::Internet.url(host: 'example.com', path: '/foobar.jpg'), description: Faker::Coffee.notes)
-  product1.save
-end
+Product.create!([
+  {name: "Spilt Delight", price: 74, image_url: "http://example.com/foobar.jpg", description: "structured, big, mint, sundried tomato, clove"},
+  {name: "Split Java", price: 64, image_url: "http://example.com/foobar.jpg", description: "wild, full, pecan, burnt sugar, green apple"},
+  {name: "Morning Pie", price: 53, image_url: "http://example.com/foobar.jpg", description: "quick, full, raspberry, leafy greens, hibiscus"},
+  {name: "Wake-up Symphony", price: 12, image_url: "http://example.com/foobar.jpg", description: "lingering, silky, honeydew, tomato, vanilla"},
+  {name: "Heart Light", price: 74, image_url: "http://example.com/foobar.jpg", description: "sharp, silky, dates, lime, mint"},
+  {name: "Veranda Mug", price: 42, image_url: "http://example.com/foobar.jpg", description: "dirty, big, lychee, hay, leathery"},
+  {name: "Captain's Town", price: 47, image_url: "http://example.com/foobar.jpg", description: "crisp, chewy, green-tea, marshmallow, corriander"},
+  {name: "Seattle Delight", price: 59, image_url: "http://example.com/foobar.jpg", description: "unbalanced, watery, black pepper, lavender, olive"},
+  {name: "Melty Enlightenment", price: 13, image_url: "http://example.com/foobar.jpg", description: "pointed, coating, tomato, lemonade, red apple"},
+  {name: "Wake-up Level", price: 87, image_url: "http://example.com/foobar.jpg", description: "balanced, silky, soil, nutella, raspberry"},
+  {name: "Normal Coffee", price: 12, image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/1200px-A_small_cup_of_coffee.JPG", description: "just coffee"},
+  {name: "Bland Coffee", price: 21, image_url: "https://i.insider.com/60d223afdb3f80001848d4c4?width=700", description: "bland coffee"}
+])
