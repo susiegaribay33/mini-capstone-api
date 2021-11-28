88bb75656c0e859e4062f154bc0235b1e4a96a0a
SusieG <susiegaribay33@gmail.com>
2021-11-28 12:09:46 -0600

    edit seeds file to fit db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
-# Product.create!([
-#   {name: "Spilt Delight", price: 74, image_url: "http://example.com/foobar.jpg", description: "structured, big, mint, sundried tomato, clove"},
-#   {name: "Split Java", price: 64, image_url: "http://example.com/foobar.jpg", description: "wild, full, pecan, burnt sugar, green apple"},
-#   {name: "Morning Pie", price: 53, image_url: "http://example.com/foobar.jpg", description: "quick, full, raspberry, leafy greens, hibiscus"},
-#   {name: "Wake-up Symphony", price: 12, image_url: "http://example.com/foobar.jpg", description: "lingering, silky, honeydew, tomato, vanilla"},
-#   {name: "Heart Light", price: 74, image_url: "http://example.com/foobar.jpg", description: "sharp, silky, dates, lime, mint"},
-#   {name: "Veranda Mug", price: 42, image_url: "http://example.com/foobar.jpg", description: "dirty, big, lychee, hay, leathery"},
-#   {name: "Captain's Town", price: 47, image_url: "http://example.com/foobar.jpg", description: "crisp, chewy, green-tea, marshmallow, corriander"},
-#   {name: "Seattle Delight", price: 59, image_url: "http://example.com/foobar.jpg", description: "unbalanced, watery, black pepper, lavender, olive"},
-#   {name: "Melty Enlightenment", price: 13, image_url: "http://example.com/foobar.jpg", description: "pointed, coating, tomato, lemonade, red apple"},
-#   {name: "Wake-up Level", price: 87, image_url: "http://example.com/foobar.jpg", description: "balanced, silky, soil, nutella, raspberry"},
-#   {name: "Normal Coffee", price: 12, image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/1200px-A_small_cup_of_coffee.JPG", description: "just coffee"},
-#   {name: "Bland Coffee", price: 21, image_url: "https://i.insider.com/60d223afdb3f80001848d4c4?width=700", description: "bland coffee"}
-# ])
+Product.create!([
+  {name: "Spilt Delight", price: 74, supplier_id: 1, description: "structured, big, mint, sundried tomato, clove"},
+  {name: "Split Java", price: 64, supplier_id: 2, description: "wild, full, pecan, burnt sugar, green apple"},
+  {name: "Morning Pie", price: 53, supplier_id: 2, description: "quick, full, raspberry, leafy greens, hibiscus"},
+  {name: "Wake-up Symphony", price: 12, supplier_id: 1, description: "lingering, silky, honeydew, tomato, vanilla"},
+  {name: "Heart Light", price: 74, supplier_id: 2, description: "sharp, silky, dates, lime, mint"},
+  {name: "Veranda Mug", price: 42, supplier_id: 1, description: "dirty, big, lychee, hay, leathery"},
+  {name: "Captain's Town", price: 47, supplier_id: 1, description: "crisp, chewy, green-tea, marshmallow, corriander"},
+  {name: "Seattle Delight", price: 59, supplier_id: 2, description: "unbalanced, watery, black pepper, lavender, olive"},
+  {name: "Melty Enlightenment", price: 13, supplier_id: 2, description: "pointed, coating, tomato, lemonade, red apple"},
+  {name: "Wake-up Level", price: 87, supplier_id: 2, description: "balanced, silky, soil, nutella, raspberry"},
+  {name: "Normal Coffee", price: 12, supplier_id: 1, description: "just coffee"},
+  {name: "Bland Coffee", price: 21, supplier_id: 1, description: "bland coffee"}
+])
 
-#OR seed dump gem
+
